98a9f176b4c05b9b69300cc0b30b336dc5aabf2b
Slijeff <jeff83636tx@gmail.com>
2022-04-15 09:55:19 -0500

    Fixed alpha-beta pruning

--- src/agents/minimax.py
+++ src/agents/minimax.py
@@ -41,7 +41,7 @@ class MinimaxAgent:
       return board.evaluate()
     if maximizingPlayer:
       value = float('-inf')
-      for move in board.legal_moves():
+      for move in board.legal_moves(True):
         value = max(value, self.alphabeta(move, depth - 1, alpha, beta, False))
         alpha = max(alpha, value)
         if beta <= alpha:
@@ -49,7 +49,7 @@ class MinimaxAgent:
       return value
     else:
       value = float('inf')
-      for move in board.legal_moves():
+      for move in board.legal_moves(False):
         value = min(value, self.alphabeta(move, depth - 1, alpha, beta, True))
         beta = min(beta, value)
         if beta <= alpha:
--- src/main.py
+++ src/main.py
@@ -4,12 +4,12 @@ import agents.minimax as mm
 game1 = ttt.TicTacToe()
 agent = mm.MinimaxAgent()
 game1.set_marker(0, 0, 'O')
-game1.set_marker(1, 1, 'X')
-game1.set_marker(0, 1, 'O')
+# game1.set_marker(1, 1, 'X')
+# game1.set_marker(0, 1, 'O')
 all_moves = game1.legal_moves(True)
 for move in all_moves:
   print(move)
   # Test one move with both true and false, if both score are 1, we will choose that?
-  print(agent.minimax(move, 10, False))
+  print(agent.alphabeta(move, 10, float('-inf'), float('inf'), False))
   # break
   # agent.minimax(move, 1, True)
